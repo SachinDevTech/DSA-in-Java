b9fcb7b08c5da36223f155bfbfcb00338c071aef
SachinDevTech <sachin.1107sk@gmail.com>
2023-12-31 11:30:48 +0530

    Merge Sort by DnQ Algo is added.

---
+++ 21_D&Q/_1_MergeSort.java
@@ -0,0 +1,58 @@
+public class _1_MergeSort {
+    public static void mergeSort(int arr[], int si, int ei) {
+        int mid = si + (ei - si) / 2;
+        if (si >= ei) {
+            return;
+        }
+        // left part
+        mergeSort(arr, si, mid);
+        // right part
+        mergeSort(arr, mid + 1, ei);
+        // merge call for the array
+        merge(arr, si, ei, mid);
+    }
+
+    public static void merge(int arr[], int si, int ei, int mid) {
+        int temp[] = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // left part
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        // right part
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+        // copy temp to original arr
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 }, si = 0, ei = arr.length - 1;
+        mergeSort(arr, si, ei);
+        printArray(arr);
+    }
+}
--- Getting Familiar with Java - Codes till 31-08-2023/DSA-in-Java-main/tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-for (int i = 0; i < row; i++) {
-        // for (int j = 1; j <=i+1 ; j++) {
-        // System.out.print(j);
-        // }
-        // System.out.println();
-
-        // }
