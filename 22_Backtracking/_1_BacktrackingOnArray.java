d305cbc42667427e45f4c0de0cdf6de7c534980a
SachinDevTech <sachin.1107sk@gmail.com>
2024-05-20 18:21:38 +0530

    Backtracking

---
+++ 22_Backtracking/_1_BacktrackingOnArray.java
@@ -0,0 +1,8 @@
+public class _1_BacktrackingOnArray {
+    public static void changeArr(int arr[], int i, int value){
+
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+    }
+}
