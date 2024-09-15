3ffc9010b675cfdc417206628fbace055f5e1a46
sachindevtech <sachin.1107sk@gmail.com>
2024-09-15 22:58:11 +0530

    Default modifier added

---
+++ 19 OOPs/OOPs_Revision/Access_Modifiers/Default_Modifier/MainStudent.java
@@ -0,0 +1,14 @@
+// package Default_Modifier;
+
+public class MainStudent {
+    public static void main(String[] args) {
+        // Creating an object of the Student class
+        Student s1 = new Student();
+        // Assigning a value to the 'name' attribute
+        s1.name = "Ashish Khyaliya";
+        
+        // Printing the name
+        System.out.println(s1.name); // Output: Ashish Khyaliya
+        System.out.println(s1.percent); // Output: 0.0
+    }
+}
---
+++ 19 OOPs/OOPs_Revision/Access_Modifiers/Default_Modifier/Student.java
@@ -0,0 +1,11 @@
+// package Default_Modifier;
+
+// 'Student' class with package-private fields
+public class Student {
+    String name;
+    int rno;
+    double percent;
+    public static void main(String[] args) {
+        
+    }
+}
